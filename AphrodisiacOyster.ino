550349c9e6f94c4dc6cb87793b026a379fb0cad5
xenoformlabs <xenoformlabs@gmail.com>
2020-01-03 15:56:05 -0800

    added button for peristaltic pump button

--- AphrodisiacOyster.ino
+++ AphrodisiacOyster.ino
@@ -10,28 +10,43 @@
 //-- pin numbers for the two pumps + LEDs that run in parallel
 #define PERISTALTIC_PUMP (4)
 #define SELF_PRIMING_PUMP (16)
+
+#define PERISTALTIC_BUTTON (5)
+#define SELF_PRIMING_BUTTON (10)
       
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting Aphrodisiac Oyster");
+  
   //-- set our two pins to output
   pinMode(PERISTALTIC_PUMP, OUTPUT);
   pinMode(SELF_PRIMING_PUMP, OUTPUT);
+
+  pinMode(PERISTALTIC_BUTTON, INPUT);
+  pinMode(SELF_PRIMING_BUTTON, INPUT);
 
   //-- Blink LEDs 3 times upon startup
   //-- we might want to change this later to have a LED indicating power only
   for( int i = 0; i < 3; i++ ) {
     digitalWrite(PERISTALTIC_PUMP, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(250);                       // wait for a second
+    delay(100);                       // wait for a second
     digitalWrite(PERISTALTIC_PUMP, LOW);    // turn the LED off by making the voltage LOW
-    delay(250);   
+    delay(100);   
   }
 }
 
 //-- track button input and activate the appropriate LED/motor
 void loop() {
-  digitalWrite(PERISTALTIC_PUMP, HIGH);  
-  digitalWrite(SELF_PRIMING_PUMP, LOW);
-  delay(1000);                       
-  digitalWrite(PERISTALTIC_PUMP, LOW);   
-  digitalWrite(SELF_PRIMING_PUMP, HIGH);
-  delay(1000);     
+  checkPeristalticPump();
+}
+
+//-- check to see if the peristaltic button is being pressed and then turn on that pump
+void checkPeristalticPump() {
+  // Check for peristantlic pump on/off
+  // (code will be more precise, later — add debounce)
+  boolean persistalticPumpOn = digitalRead(PERISTALTIC_BUTTON);
+  if( persistalticPumpOn )
+    digitalWrite(PERISTALTIC_PUMP, HIGH); 
+  else  
+    digitalWrite(PERISTALTIC_PUMP, LOW);  
 }
