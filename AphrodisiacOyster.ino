1e1fb9761b8844ec886759699f597b2735771934
xenoformlabs <xenoformlabs@gmail.com>
2020-01-07 22:28:56 -0800

    added Circuit Playground code

--- AphrodisiacOyster.ino
+++ AphrodisiacOyster.ino
@@ -7,17 +7,25 @@
   transistor is also connected in parallel to an LED.
 */
 
-//-- pin numbers for the two pumps + LEDs that run in parallel
-#define PERISTALTIC_PUMP (4)
-#define SELF_PRIMING_PUMP (16)
+#include <Adafruit_CircuitPlayground.h>
 
-#define PERISTALTIC_BUTTON (5)
-#define SELF_PRIMING_BUTTON (18)
-      
+//-- pin numbers for the two pumps + LEDs that run in parallel
+#define PERISTALTIC_PUMP (6)
+#define SELF_PRIMING_PUMP (12)
+
+#define PERISTALTIC_BUTTON (10)
+#define SELF_PRIMING_BUTTON (9)
+
+// global var for brightness for Neopixels
+int brightness = 255;
+int brightDir = -1;
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting Aphrodisiac Oyster");
-  
+
+  CircuitPlayground.begin();
+   
   //-- set our two pins to output
   pinMode(PERISTALTIC_PUMP, OUTPUT);
   pinMode(SELF_PRIMING_PUMP, OUTPUT);
@@ -27,11 +35,24 @@ void setup() {
 
   //-- Blink LEDs 3 times upon startup
   //-- we might want to change this later to have a LED indicating power only
-  for( int i = 0; i < 3; i++ ) {
+  /*for( int i = 0; i < 3; i++ ) {
     digitalWrite(PERISTALTIC_PUMP, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(100);                       // wait for a second
     digitalWrite(PERISTALTIC_PUMP, LOW);    // turn the LED off by making the voltage LOW
     delay(100);   
+  }*/
+
+  for( int i = 0; i < 5; i++ ) {
+    CircuitPlayground.redLED(HIGH);
+    delay(100);
+    CircuitPlayground.redLED(LOW);
+    delay(100);   
+  }
+  CircuitPlayground.redLED(HIGH);
+
+  // 10 built-in NeoPixels, set all to blue
+  for( int i = 0; i < 10; i++ ) {
+    CircuitPlayground.setPixelColor(i, 0,0,255);
   }
 }
 
@@ -39,6 +60,9 @@ void setup() {
 void loop() {
   checkPeristalticPump();
   checkSelfPrimingPump();
+
+  // add later for pulsating effect
+ // adjustBrightness();
 }
 
 //-- check to see if the peristaltic button is being pressed and then turn on that pump
@@ -62,3 +86,19 @@ void checkSelfPrimingPump() {
   else  
     digitalWrite(SELF_PRIMING_PUMP, LOW);  
 }
+
+/*
+void adjustBrightness() {
+  brightness += brightDir;
+  if(  brightness < 0 ) {
+    brightness = 0;
+    brightDir = -brightDir;
+  }
+  else if( brightness > 255 ) {
+    brightness = 255;
+    brightDir = -brightDir;
+  } 
+
+  CircuitPlayground.setBrightness(brightness);
+}
+*/
