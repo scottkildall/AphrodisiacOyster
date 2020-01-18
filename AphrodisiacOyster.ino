646afb13f7fe98590b178ef0e6046393ca0643b7
xenoformlabs <xenoformlabs@gmail.com>
2020-01-18 12:07:02 -0800

    cleaner code update

--- AphrodisiacOyster.ino
+++ AphrodisiacOyster.ino
@@ -12,13 +12,15 @@
 //-- pin numbers for the two pumps + LEDs that run in parallel
 #define PERISTALTIC_PUMP (6)
 #define SELF_PRIMING_PUMP (12)
+#define AIR_PUMP (3)
 
 #define PERISTALTIC_BUTTON (10)
 #define SELF_PRIMING_BUTTON (9)
+#define AIR_BUTTON (2)
 
 // global var for brightness for Neopixels
-int brightness = 255;
-int brightDir = -1;
+int brightness = 128;
+int brightDir = 1;
 
 void setup() {
   Serial.begin(115200);
@@ -29,9 +31,12 @@ void setup() {
   //-- set our two pins to output
   pinMode(PERISTALTIC_PUMP, OUTPUT);
   pinMode(SELF_PRIMING_PUMP, OUTPUT);
+  pinMode(AIR_PUMP, OUTPUT);
 
   pinMode(PERISTALTIC_BUTTON, INPUT);
   pinMode(SELF_PRIMING_BUTTON, INPUT);
+  pinMode(AIR_BUTTON, INPUT);
+
 
   //-- Blink LEDs 3 times upon startup
   //-- we might want to change this later to have a LED indicating power only
@@ -60,9 +65,11 @@ void setup() {
 void loop() {
   checkPeristalticPump();
   checkSelfPrimingPump();
+  checkAirPump();
 
   // add later for pulsating effect
- // adjustBrightness();
+  adjustBrightness();
+  delay(20);
 }
 
 //-- check to see if the peristaltic button is being pressed and then turn on that pump
@@ -87,18 +94,32 @@ void checkSelfPrimingPump() {
     digitalWrite(SELF_PRIMING_PUMP, LOW);  
 }
 
-/*
+//-- check to see if the air button is being pressed and then turn on that pump
+void checkAirPump() {
+  // Check for self-priming pump on/off
+  // (code will be more precise, later — add debounce)
+  boolean pumpOn = digitalRead(AIR_BUTTON);
+  if( pumpOn )
+    digitalWrite(AIR_PUMP, HIGH); 
+  else  
+    digitalWrite(AIR_PUMP, LOW);  
+}
+
+
 void adjustBrightness() {
-  brightness += brightDir;
-  if(  brightness < 0 ) {
-    brightness = 0;
-    brightDir = -brightDir;
+  brightness = brightness + brightDir;
+  if( brightness < 128 ) {
+    brightness = 128;
+    brightDir = 1;    // start fading up
   }
   else if( brightness > 255 ) {
     brightness = 255;
-    brightDir = -brightDir;
+    brightDir = -1;   // start fading down
   } 
 
+
   CircuitPlayground.setBrightness(brightness);
+   for( int i = 0; i < 10; i++ ) {
+    CircuitPlayground.setPixelColor(i, 0,0,255);
+  }
 }
-*/
